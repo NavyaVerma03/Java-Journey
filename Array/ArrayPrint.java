4ca3a7ef294b2de70b3d6aa37abac5bfdac23cfa
Navya Verma <navyaverma451@gmail.com>
2026-06-30 20:17:17 +0530

    Initial Commit

---
+++ Array/ArrayPrint.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArrayPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i+1)+" :");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Array = [");
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i]);
+
+            if (i != size - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println("]");
+    }
+    
+}
